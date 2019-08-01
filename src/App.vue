14cf01e50a17dda2a2d5ae301aeef2fcacaf0103
Koeri Kagura <qianwumeng_2cyndi@qq.com>
2019-08-01 18:38:47 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Canvas></Canvas>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Canvas from './components/Canvas';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Canvas
   }
 }
 </script>
@@ -23,6 +22,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
