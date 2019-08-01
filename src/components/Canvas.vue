b6638459cd6683936babe7a23cd5d73c849b3cc2
Koeri Kagura <qianwumeng_2cyndi@qq.com>
2019-08-01 18:39:01 +0800

    Create Canvas.vue

---
+++ src/components/Canvas.vue
@@ -0,0 +1,37 @@
+<template>
+ <div>
+   <canvas id="canvas"></canvas>
+ </div>
+</template>
+
+<script>
+ export default {
+   data () {
+     return {
+       background: '#444'
+     }
+   },
+   components: {
+
+   },
+   mounted() {
+      const ctx = document.getElementById('canvas').getContext('2d');
+      ctx.rect(0,0,700,176);
+      ctx.stroke();
+      /* eslint-disable */
+      console.log('a');
+      ctx.fillStyle = this.$data.background;
+      ctx.fillRect(0,0,700,32);
+   }
+ }
+</script>
+
+<style lang="less" scoped>
+#canvas{
+  width: 700px;
+  height: 176px;
+  border: 1px solid #444;
+  margin: 4rem;
+}
+ 
+</style>
