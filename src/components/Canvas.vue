bd96edb4753c0f8a7fd39a96f7b597b805a441b7
Koeri Kagura <qianwumeng_2cyndi@qq.com>
2019-08-02 18:49:45 +0800

    Update Canvas.vue

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -1,6 +1,6 @@
 <template>
  <div>
-   <canvas id="canvas"></canvas>
+   <canvas id="canvas" width="700px" height="168px"></canvas>
  </div>
 </template>
 
@@ -8,7 +8,11 @@
  export default {
    data () {
      return {
-       background: '#444'
+      background: '#444',
+      chMainTitle: '科韵路',
+      enTitle: 'Fo Wan Lou',
+      chNextStation: '员村',
+      enNextStation: 'Yuancun'
      }
    },
    components: {
@@ -16,18 +20,36 @@
    },
    mounted() {
       const ctx = document.getElementById('canvas').getContext('2d');
-      ctx.rect(0,0,700,176);
-      ctx.stroke();
+      // 绘制背景
+      ctx.rect(0,0,700,168);
       ctx.fillStyle = this.$data.background;
-      ctx.fillRect(0,0,700,32);
+      ctx.fillRect(0,148,700,24);
+      // 编译完毕记得删掉下面的
+      ctx.strokeStyle = 'blue';
+      ctx.moveTo(350,0);
+      ctx.lineTo(350,150);
+      ctx.stroke();
+      // 编译完毕记得删掉上面的
+      // 绘制正中站点
+      ctx.fillStyle = '#000';
+      ctx.textAlign = 'center';
+      ctx.font = '40px bolder Arial';
+      ctx.fillText(this.$data.chMainTitle,350,70);
+      ctx.font = '22px bolder Arial';
+      ctx.fillText(this.$data.enTitle,350,102);
+      // 绘制下一站
+      ctx.font = '18px bolder Arial';
+      ctx.fillText('下站',56,72);
+      ctx.fillText(this.$data.chNextStation,108,72);
+      ctx.font = '10px bolder Arial';
+      ctx.fillText('Next',56,86);
+      ctx.fillText(this.$data.enNextStation,108,86);
    }
  }
 </script>
 
 <style lang="less" scoped>
 #canvas{
-  width: 700px;
-  height: 176px;
   border: 1px solid #444;
   margin: 4rem;
 }
