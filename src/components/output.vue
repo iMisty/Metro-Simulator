f1024841b8e3f9268d5985ef6563d3940c845e38
Miya <qianwumeng_2cyndi@qq.com>
2019-09-14 22:56:39 +0800

    Create output.vue

---
+++ src/components/output.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="output">
+  <article>生成签名图</article>
+</div>
+</template>
+
+<script>
+export default{
+  data(){
+    name: 'output'
+    return{
+      
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+</style>
