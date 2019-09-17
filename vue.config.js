1c99a62f08c3b88032bf6a041744fc3608944500
Miya <qianwumeng_2cyndi@qq.com>
2019-09-18 01:17:54 +0800

    Create vue.config.js
    调整打包路径

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  baseUrl: './',
+  outputDir: 'dist',
+  lintOnSave: true,
+  runtimeCompiler: true, 
+  chainWebpack: () => {},
+  configureWebpack: () => {},
+  devServer: {
+    open: process.platform === 'darwin',
+    host: '0.0.0.0',
+    port: 8080,
+    https: false,
+    hotOnly: false,
+    proxy: null, // string | Object
+    before: app => {}
+  }
+}
