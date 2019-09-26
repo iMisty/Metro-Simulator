7272e98156981801a8be882b4e5638d0857c4db9
Miya <qianwumeng_2cyndi@qq.com>
2019-09-27 00:41:07 +0800

    Update store.ts

--- src/store.ts
+++ src/store.ts
@@ -5,7 +5,16 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    chThisStation: '芝士橙',
+    enThisStation: 'Sino-Singapore Guangzhou Knowledge City',
+    chNextStation: '枫下',
+    enNextStation: 'Fengxia',
+    Line: '14',
+    nowStation: '40',
+    platform: '4',
+    color: 'line14',
+    htmlUrl: '',
+    isAPM: false,
   },
   mutations: {
 
