9d08a6b00f6481a3ddb4275baae3ed9490657d06
Miya <qianwumeng_2cyndi@qq.com>
2019-09-27 00:41:12 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,13 @@
 import Vue from 'vue';
 import App from './App.vue';
-// import router from './router';
+import router from './router';
 // import store from './store';
-import './assets/less/line.less';
-import './assets/less/global.less';
-import './assets/less/left.less';
-import './assets/less/medium.less';
-import './assets/less/right.less';
-import './assets/less/options.less';
+import './assets/less/style.less';
 
 Vue.config.productionTip = false;
 
 new Vue({
-  // router,
+  router,
   // store,
   render: (h) => h(App),
 }).$mount('#app');
