bae5e197deb60bf2852be7b35f141332be37e91e
Miya <qianwumeng_2cyndi@qq.com>
2019-09-27 00:41:17 +0800

    Update copyright.vue

--- src/components/copyright.vue
+++ src/components/copyright.vue
@@ -1,5 +1,8 @@
 <template>
-
+  <div class="copyright">
+      <h3>2019 Wednesday Teatime.Made by Koeri</h3>
+      <a href="https://github.com/iMisty/GM-Simulator">开源于GitHub</a>
+    </div>
 </template>
 
 <script lang="ts">
