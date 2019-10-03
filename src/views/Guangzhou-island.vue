161ef656d5db4555b41bff5328e3b485ab33d374
Miya <qianwumeng_2cyndi@qq.com>
2019-10-04 04:43:40 +0800

    Create: Test Route

---
+++ src/views/Guangzhou-island.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  GuangzhouIsland
+</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
+@Component({
+components:{
+
+}
+})
+export default class GuangzhouIsland extends Vue{
+
+}
+</script>
+<style lang="less" scoped>
+</style>
