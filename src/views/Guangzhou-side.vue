2354f89ffbd66f77339d1e9458248a8f2efb369e
Miya <qianwumeng_2cyndi@qq.com>
2019-10-04 04:43:53 +0800

    Create: New Routes

---
+++ src/views/Guangzhou-side.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  GuangzhouSide
+</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
+@Component({
+components:{
+
+}
+})
+export default class GuangzhouSide extends Vue{
+
+}
+</script>
+<style lang="less" scoped>
+</style>
