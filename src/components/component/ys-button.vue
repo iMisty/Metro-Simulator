2ee2e641e42260ca901a873800893422c29888b9
Miya <qianwumeng_2cyndi@qq.com>
2019-10-07 21:28:57 +0800

    fixed: change prop title and color

--- src/components/component/ys-button.vue
+++ src/components/component/ys-button.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="ys-buttons">
-    <button class="ys-button">{{buttonText}}</button>
+    <button class="ys-button">{{title}}</button>
   </div>
 </template>
 
@@ -13,14 +13,15 @@ export default class ysButton extends Vue {
   @Prop({
     type: String,
     required: true,
-    default: "Default Value"
+    default: "Button"
   })
-  buttonText!: String;
+  title!: String;
 }
 </script>
 <style lang="less" scoped>
 .ys-button {
   position: relative;
+  margin: 8px;
   padding: 0 16px;
   min-width: 88px;
   height: 36px;
@@ -30,10 +31,10 @@ export default class ysButton extends Vue {
   border: none;
   outline: none;
   text-transform: uppercase;
-  background-color: #E040FB;
+  background-color: #673ab7;
   color: #fff;
   &:hover{
-    background-color: lighten(#E040FB,8%);
+    background-color: lighten(#673ab7,8%);
   }
 }
 </style>
