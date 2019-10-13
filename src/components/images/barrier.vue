8e9e180a5318606d5e3c19dda61bbb852731d5aa
Miya <qianwumeng_2cyndi@qq.com>
2019-10-13 23:12:20 +0800

    Create barrier.vue

---
+++ src/components/images/barrier.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="barrier flex column">
+      <h1 class="barrier-num">{{num}}</h1>
+      <h3>屏蔽门</h3>
+    </div>
+</template>
+
+<script>
+
+export default {
+    components: {},
+    data() {
+        return {
+
+        };
+    },
+    computed: {},
+    watch: {},
+    methods: {},
+    created() {},
+    mounted() {},
+    beforeCreate() {},
+    beforeMount() {},
+    beforeUpdate() {},
+    updated() {},
+    beforeDestroy() {},
+    destroyed() {},
+    activated() {},
+    }
+</script>
+<style lang='less' scoped>
+//@import url(); 引入公共css类
+
+</style>
