d2e9ffdd25811d0a12760436f67139e6705fa7d7
Koeri Kagura <qianwumeng_2cyndi@qq.com>
2019-10-15 17:47:38 +0800

    fixed: delete bus.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,24 +1,24 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    chThisStation: '知识城',
-    enThisStation: 'Sino-Singapore Guangzhou Knowledge City',
-    chNextStation: '枫下',
-    enNextStation: 'Fengxia',
-    Line: '14',
-    StationNumber: '40',
-    platform: '4',
-    color: 'line14',
-    barrier: '4'
-  },
-  mutations: {
+    state: {
+        chThisStation: '知识城',
+        enThisStation: 'Sino-Singapore Guangzhou Knowledge City',
+        chNextStation: '枫下',
+        enNextStation: 'Fengxia',
+        Line: '14',
+        StationNumber: '40',
+        platform: '4',
+        color: 'line14',
+        barrier: '4'
+    },
+    mutations: {
 
-  },
-  actions: {
+    },
+    actions: {
 
-  }
+    }
 })
